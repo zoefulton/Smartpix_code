f65a1a3c05b92f3f88b3522dd355586ddb5afc22
zoefulton <zoe.cs.fulton@gmail.com>
2026-03-30 20:49:13 -0500

    changes to smartpix_file to make more neat and easy to use as package

--- smartpix_file.py
+++ smartpix_file.py
@@ -1,40 +1,39 @@
-humidity_file = open('/Users/zoefulton/Downloads/Smartpix_files/Smartpix_code/test_20260326.txt', 'r', encoding='latin-1')  # need to change this to path on lab computer
-humidity_data = humidity_file.read()
-
-def last_humidity_value(file):
-    # find last line of humidity data
-    humidity_lines = file.splitlines()
-    last_line = humidity_lines[len(humidity_lines)-1]
-
-    # split last line into list of characters
-    last_line_split = list(last_line)
-    last_5_characters = last_line_split[-5:]
-
-    # join last 5 characters into string and convert it into a integer
-    last_5_characters = ''.join(last_5_characters)
-    last_5_characters = float(last_5_characters)
-    return last_5_characters
-
-humidity = last_humidity_value(humidity_data)
-
-#functions to turn on and off switch 1 based on humidity value
 import serial
 import serial.tools.list_ports
+file = '/Users/zoefulton/Downloads/Smartpix_files/Smartpix_code/test_20260326.txt'
+ser = serial.Serial("/dev/cu.usbserial-11210", 9600, timeout=1) # will need to change device name on lab computer
 
-ports = serial.tools.list_ports.comports()
+def read_humidity_control_switch(file, humidity):
+    humidity_file = open(file, 'r', encoding='latin-1')  # need to change this to path on lab computer
+    humidity_data = humidity_file.read()
 
-def on_switch_1():
-    ser = serial.Serial("/dev/cu.usbserial-11210", 9600, timeout=1) # will need to change device name on lab computer
-    data = bytearray([0xA0, 0x01, 0x01, 0xA2])
-    ser.write(data)
-    return "Switch 1 ON"
+    # get last humidity value from file
+    def last_humidity_value(file):
+    # find last line of humidity data
+        humidity_lines = file.splitlines()
+        last_line = humidity_lines[len(humidity_lines)-1]
+        humidity_string = last_line.split("\t")[4]
+        humidity = float(humidity_string)
+        return humidity
+    
+    # define humidity threshold for turning on/off switch
+    def humidity_threshold(humidity):
+        return 50.0
 
-def off_switch_1():
-    ser = serial.Serial("/dev/cu.usbserial-11210", 9600, timeout=1)
-    data = bytearray([0xA0, 0x01, 0x00, 0xA1])
-    ser.write(data)
-    return "Switch 1 OFF"
+    #functions to turn on and off switch 1 based on humidity value
+    #ports = serial.tools.list_ports.comports()
 
-if humidity >= 50:
-    off_switch_1()
-    print("Humidity has reached threshold level, turning off electronics.")
+    def on_switch_1():
+        data = bytearray([0xA0, 0x01, 0x01, 0xA2])
+        ser.write(data)
+        return "Switch 1 ON"
+
+    def off_switch_1():
+        data = bytearray([0xA0, 0x01, 0x00, 0xA1])
+        ser.write(data)
+        return "Switch 1 OFF"
+
+    if humidity >= humidity_threshold(humidity):
+        off_switch_1()
+        print("Humidity has reached threshold level, turning off electronics.")
+    

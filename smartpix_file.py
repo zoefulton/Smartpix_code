f44a880079aa1ff0fe212d32449711e3b3f4e8b1
daq <daq@kdplab02.ad.uchicago.edu>
2026-04-02 14:56:37 -0500

    Changes to function to find humidity threshold, need to debug (will do on laptop)

--- smartpix_file.py
+++ smartpix_file.py
@@ -1,39 +1,56 @@
 import serial
 import serial.tools.list_ports
-file = "Users/daq/Downloads/test_28260330.txt"
+import math
+file = "C:/Users/daq/Downloads/test_20260330.txt"
 ser = serial.Serial("COM3", 9600, timeout=1) # will need to change device name on lab computer
+chip_temperature = -22
 
-def read_humidity_control_switch(file, humidity, temperature):
-    humidity_file = open(file, 'r', encoding='latin-1')  # need to change this to path on lab computer
-    humidity_data = humidity_file.read()
+def read_humidity_control_switch(file_str, ser, chip_temperature):
 
+    file = open(file_str, 'r', encoding='latin-1')  # need to change this to path on lab computer
+    humidity_data = file.read()
+#need to go now, but will change everything from file to humidity_data (i think that's the problem) taking switch so can run on own computer
     # get last humidity value from file
-    def last_humidity_value(file):
-    # find last line of humidity data
+    def temperature_humidity_value(file):
+        file = file.read()
         humidity_lines = file.splitlines()
         last_line = humidity_lines[len(humidity_lines)-1]
+        print("hellow")
+        print(last_line)
         humidity_string = last_line.split("\t")[4]
         humidity = float(humidity_string)
-        return humidity
-    
+
+        temperature_string = last_line.split("\t")[3] #need to check specific position of temperature
+        temperature = float(temperature_string)
+        return humidity, temperature,
+
+
     # define humidity threshold for turning on/off switch
-    def humidity_threshold(humidity):
-        return temperature-((100-humidity)/5)
+    def dry_temperature(file):
+        humidity, temperature = temperature_humidity_value(file)
+        b, c = 17.625, 243.04
+        gamma = math.log(humidity/100) + (b*temperature)/(c+temperature)        
+        T_dry = (c*gamma)/(b-gamma)
+        return T_dry
 
     #functions to turn on and off switch 1 based on humidity value
     #ports = serial.tools.list_ports.comports()
 
-    def on_switch_1():
+    def on_switch_1(ser):
         data = bytearray([0xA0, 0x01, 0x01, 0xA2])
         ser.write(data)
         return "Switch 1 ON"
 
-    def off_switch_1():
+    def off_switch_1(ser):
         data = bytearray([0xA0, 0x01, 0x00, 0xA1])
         ser.write(data)
         return "Switch 1 OFF"
 
-    if humidity >= humidity_threshold(humidity):
+    if dry_temperature(file)+2 <= chip_temperature:
         off_switch_1()
         print("Humidity has reached threshold level, turning off electronics.")
+    else:
+        print(f"Humidity/temperature is: {temperature_humidity_value(file)} so dewpoint is {dry_temperature(file)}")
+
+print(read_humidity_control_switch(file, ser, chip_temperature))
   
